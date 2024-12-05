c0f374603dd24cdb94efc081ec8c7cfee13cd648
arghya339 <arghyadeepmondal339@duck.com>
2024-12-05 20:34:14 +0530

    Add: YT Music RVX & YT Music Beta  A7 & A5-6 forked  by @inotia00

--- src/build/Revanced-Extended-Beta.sh
+++ src/build/Revanced-Extended-Beta.sh
@@ -63,6 +63,54 @@ revanced_dl(){
 	split_editor "youtube-lite-beta" "youtube-lite-beta-armeabi-v7a" "include" "split_config.armeabi_v7a split_config.en split_config.xhdpi split_config.xxxhdpi"
 	patch "youtube-lite-beta-armeabi-v7a" "revanced-extended" "inotia"
 }
+4() {
+	revanced_dl
+	# Patch YouTube Music Extended for android 7:
+	# Arm64-v8a
+	get_patches_key "youtube-music-revanced-extended"
+	version="6.42.55"
+	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-android-7-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
+	patch "youtube-music-beta-android-7-arm64-v8a" "revanced-extended" "inotia"
+	# Armeabi-v7a
+	get_patches_key "youtube-music-revanced-extended"
+	version="6.42.55"
+	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-android-7-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
+	patch "youtube-music-beta-android-7-armeabi-v7a" "revanced-extended" "inotia"
+	# x86
+	get_patches_key "youtube-music-revanced-extended"
+	version="6.42.52"
+	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-android-7-x86 "youtube-music" "google-inc/youtube-music/youtube-music" "x86"
+	patch "youtube-music-beta-android-7-x86" "revanced-extended" "inotia"
+	# x86_64
+	get_patches_key "youtube-music-revanced-extended"
+	version="6.42.52"
+	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-android-7-x86_64" "youtube-music" "google-inc/youtube-music/youtube-music" "x86_64"
+	patch "youtube-music-beta-android-7-x86_64" "revanced-extended" "inotia"
+}
+5() {
+	revanced_dl
+	# Patch YouTube Music Extended for android 5 & 6:
+	# Arm64-v8a
+	get_patches_key "youtube-music-revanced-extended"
+	version="6.20.51"
+	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-android-5-6-arm64-v8a" "youtube-music" "google-inc/youtube-music/youtube-music" "arm64-v8a"
+	patch "youtube-music-beta-android-5-6-arm64-v8a" "revanced-extended" "inotia"
+	# Armeabi-v7a
+	get_patches_key "youtube-music-revanced-extended"
+	version="6.20.51"
+	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-android-5-6-armeabi-v7a" "youtube-music" "google-inc/youtube-music/youtube-music" "armeabi-v7a"
+	patch "youtube-music-beta-android-5-6-armeabi-v7a" "revanced-extended" "inotia"
+	# x86
+	get_patches_key "youtube-music-revanced-extended"
+	version="6.20.51"
+	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-android-5-6-x86" "youtube-music" "google-inc/youtube-music/youtube-music" "x86"
+	patch "youtube-music-beta-android-5-6-x86" "revanced-extended" "inotia"
+	# x86_64
+	get_patches_key "youtube-music-revanced-extended"
+	version="6.20.51"
+	get_apk "com.google.android.apps.youtube.music" "youtube-music-beta-android-5-6-x86_64" "youtube-music" "google-inc/youtube-music/youtube-music" "x86_64"
+	patch "youtube-music-beta-android-5-6-x86_64" "revanced-extended" "inotia"
+}
 case "$1" in
     1)
         1
@@ -72,5 +120,11 @@ case "$1" in
         ;;
     3)
         3
+        ;;
+    4)
+        4
+        ;;
+    5)
+        5
         ;;
 esac
